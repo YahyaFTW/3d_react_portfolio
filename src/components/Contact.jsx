dcfc2ae2a9cc84d1087b507d339ada116ec836b8
YahyaFTW <syedyahya010@gmail.com>
2023-07-24 00:27:52 +0530

    env added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// service_70ymhtq
+// template_dycx30l
+// t6lgQQedoK_64hqNG
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -37,9 +41,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Syed Yahya Ahmad",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "syedyahya010@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
